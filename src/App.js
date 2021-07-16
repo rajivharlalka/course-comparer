6fe5f8f7b2e186b657bfc88d023431d86f2729cd
cghosh828049 <cghosh828049@gmail.com>
2021-07-16 19:26:07 +0530

    Created a basic skeleton of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Branch from './Branch/Branch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    compareToggle : false
+  }
+
+  showCourses = (event) => {
+    event.preventDefault();
+    console.log("Testing"); //To prevent reload of page upon submission
+  }
+
+  changeMode = () => {
+    const tempCompareToggle = this.state.compareToggle;
+    this.setState({
+      compareToggle : !tempCompareToggle
+    });
+  };
+
+
+  render() {
+
+    var compare = (
+      <div className="maindiv">
+        <Branch submit={(event) => this.showCourses(event)}></Branch>
+      </div>
+    );
+
+    if(this.state.compareToggle) {
+      compare = (
+        <div className="maindiv">
+          <Branch submit={(event) => this.showCourses(event)}></Branch>
+          <Branch submit={(event) => this.showCourses(event)}></Branch>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>IITKGP Course Comparer</h1>
+        <label>Compare : &nbsp;
+          <input type="checkbox" onChange={this.changeMode}></input>
+        </label>
+        {compare}
+      </div>
+    );
+  }
 }
 
 export default App;
