a605dca00e287ee95a00971693974a7e389065c7
rajiv_harlalka <rajivharlalka009@gmail.com>
2021-07-17 11:45:37 +0530

    Sem wise data populated in array of arrays

--- src/Branch/Branch.js
+++ src/Branch/Branch.js
@@ -3,11 +3,11 @@ import Select from "react-select";
 import "./Branch.css";
 import dep from "./../data/dep";
 import branch from "./../data/dep-branch";
-import course from './../data/course';
+import course from "./../data/course";
 
 const Branch = (props) => {
   const [branches, updateBranches] = useState([]);
-//   const [Value, setValue] = useState("Select dep and Course")
+  const [Text, setText] = useState({ text: "Select dep and Course" });
 
   var departments = dep.department.map((e) => {
     return { label: e, value: e };
@@ -18,18 +18,26 @@ const Branch = (props) => {
       return { label: val, value: val };
     });
     updateBranches(br);
-    console.log(branches);
+    // console.log(branches);
   }
 
   function handleDepChange(e) {
     updateBranch(e.value);
-    console.log(e.value);
+    // console.log(e.value);
   }
 
-  function handleCourseChange(e){
-    // setValue((course[e.value]).toString);
-    console.log(course[e.value]);
+  function handleCourseChange(e) {
+    setText(course[e.value]);
   }
+  
+  var semdata;
+  function getSemData() {
+    var entity = Object.keys(Text);
+    semdata = entity.map((e) => {
+      return Text[e];
+    });
+  }
+  getSemData();
 
   return (
     <div className='Branch'>
@@ -44,11 +52,17 @@ const Branch = (props) => {
         </Select>
         <br />
         <label> Select Branch : </label>
-        <Select options={branches} className='dropdown' onChange={(e)=> handleCourseChange(e)}></Select>
+        <Select
+          options={branches}
+          className='dropdown'
+          onChange={(e) => handleCourseChange(e)}
+        ></Select>
         <br />
         <button onClick={props.submit}>Submit</button>
       </form>
-      <div className='courseDisplay'>Add dept and course</div>
+      <div className='courseDisplay'>
+        {console.log(semdata)}
+      </div>
     </div>
   );
 };
